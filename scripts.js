221d05a37eee1783fd9ca85b7b17ed566449432c
Peter <peterhahn@Peters-MBP.fios-router.home>
2019-06-11 21:46:04 -0400

    injury

--- scripts.js
+++ scripts.js
@@ -38,16 +38,16 @@ jQuery(function ($) {
             "name": "4. 8 BIT NEMESIS",
             "length": "01:49",
             "file": "8Bit"
-        },  {
-            "track": 5,
-            "name": "5. (INTERLUDE - BINTANG)",
-            "length": "02:53",
-            "file": "Bintang"
         }, {
-            "track": 6,
-            "name": "6. PROGRAM",
+            "track": 5,
+            "name": "5. PROGRAM",
             "length": "02:36",
             "file": "Program"
+        }, {
+            "track": 6,
+            "name": "6. (INTERLUDE - BINTANG)",
+            "length": "02:53",
+            "file": "Bintang"
         }, {
             "track": 7,
             "name": "7. ENEMY SONG",
