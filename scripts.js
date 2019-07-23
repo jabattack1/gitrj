2f61991edf3821bc9d22d0950dbfb334b3b5527f
Peter <peterhahn@Peters-MBP.fios-router.home>
2019-07-23 16:11:28 -0400

    bore

--- scripts.js
+++ scripts.js
@@ -41,7 +41,7 @@ jQuery(function ($) {
         }, {
             "track": 5,
             "name": "5. I'M THE FIRE",
-            "length": "02:31",
+            "length": "02:20",
             "file": "Fire"
         }, {
             "track": 6,
