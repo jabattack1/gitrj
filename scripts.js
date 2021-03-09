2eac2c200c55f455b56e832903242894caf30c10
Peter <peterhahn@Peters-MacBook-Pro.local>
2021-03-09 15:19:49 +0900

    touch

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ jQuery(function ($) {
         },  {
             "track": 6,
             "name": "6. WACKO",
-            "length": "01:48",
+            "length": "01:51",
             "file": "Wacko"
         }, {
             "track": 7,
